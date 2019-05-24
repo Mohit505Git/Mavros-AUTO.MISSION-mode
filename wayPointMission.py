11be2fa406eeb6dd78b2e73d10342957068170cf
Mohit505git <mohitsiitkgp17@gmail.com>
2019-05-24 19:39:12 +0530

    Second commit for Mission mode

--- wayPointMission.py
+++ wayPointMission.py
@@ -9,7 +9,7 @@ from mavros_msgs.msg import *
 from mavros_msgs.srv import *
 
 
-class Modes(self):
+class Modes:
     def __init__(self):
         pass
 
@@ -25,8 +25,8 @@ class Modes(self):
         rospy.wait_for_service('mavros/set_mode')
         try:
             # setModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.set_mode.request.custom_mode)
-            setModeService = rospy.ServiceProxy('mavros/set_mode', set_mode.request.custom_mode)
-            setModeService("AUTO.MISSION")
+            setModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.SetMode)
+            setModeService(custom_mode="AUTO.MISSION")
         except rospy.ServiceException, e:
             print "Service takeoff call failed: %s"%e
 
@@ -34,33 +34,77 @@ class Modes(self):
         rospy.wait_for_service('mavros/mission/push')
         try:
             wpPushService = rospy.ServiceProxy('mavros/mission/push', WaypointPush,persistent=True)
-            wpPushService(wps)
+            wpPushService(start_index=0,waypoints=wps)
         except rospy.ServiceException, e:
             print "Service takeoff call failed: %s"%e
 
 
-class wpMissionCnt(self):
+class wpMissionCnt:
     
     def __init__(self):
         self.state = State()
         # Instantiate a setpoints message
         self.sp = PositionTarget()
 
-        self. = PositionTarget()
         # set the flag to use position setpoints and yaw angle
         self.sp.type_mask = int('010111111000', 2)
         # LOCAL_NED
         self.sp.coordinate_frame = 1
 
+        self.wp = Waypoint()
+
+
     def stateCb(self, msg):
         self.state = msg
 
+    def setWaypoints1(self):
+        self.wp.frame ="FRAME_LOCAL_NED"
+        self.wp.command = "NAV_TAKEOFF"
+        self.wp.is_current= True
+        self.wp.autocontinue = True
+        # self.wp.param1=
+        # self.wp.param2=
+        # self.wp.param3=
+        # self.wp.param4=
+        self.wp.x_lat= 20
+        self.wp.y_long= 20 
+        self.wp.z_alt= 3
+        return self.wp
+
+    def setWaypoints2(self):
+        self.wp.frame ="FRAME_LOCAL_NED"
+        self.wp.command = "NAV_RETURN_TO_LAUNCH"
+        self.wp.is_current= False
+        self.wp.autocontinue = True
+        # self.wp.param1=
+        # self.wp.param2=
+        # self.wp.param3=
+        # self.wp.param4=
+        self.wp.x_lat= 0
+        self.wp.y_long= 0 
+        self.wp.z_alt= 0
+        return self.wp
+
 
 def main():
-    
     rospy.init_node('waypointMission', anonymous=True)
-    rospy.Subscriber("/mavros/state",State, wpMissionCnt.stateCb)
+    rate = rospy.Rate(20.0)
 
+    md = Modes()
+    wayp = wpMissionCnt()
+    wps = []
+    wps.append(wayp.setWaypoints1())
+    # wps.append(wayp.setWaypoints2())
+    md.wpPush(wps)
+    rospy.Subscriber("/mavros/state",State, wayp.stateCb)
+
+
+    while not wayp.state.armed:
+        md.setArm()
+        rate.sleep()
+    md.auto_set_mode()
+
+    rospy.spin()
 
 
 if __name__ == '__main__':
@@ -68,4 +112,3 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-
